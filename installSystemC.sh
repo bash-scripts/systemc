f8046b390aadfddcf57cf5d33fdcb0779e6b4455
Georgy <31808871+kormotodor@users.noreply.github.com>
2017-12-06 07:53:48 +0300

    update installSystemC.sh
    changes:
      -add notifications about actions;
      -delete verbose (-v) key from "tar";
      -unnecessary directories and archives now are self-destructible;
      -specified the target file for "sed";
      -change the way of redacting the "/etc/environment" to "tee";
      -little cosmetic fixes.

--- installSystemC.sh
+++ installSystemC.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-digit=64
-sudo -i
+digit=64  #it can be scan with "uname -m"
+echo "Welcome to SystemC installer!"
+echo "Downloading..."
 wget http://www.accellera.com/images/downloads/standards/systemc/systemc-2.3.2.tar.gz
-tar -xvFj systemc-2.3.2.tar.gz
+echo "Decompression..."
+tar -xf systemc-2.3.2.tar.gz
+echo "Deleting archive..."
+rm systemc-2.3.2.tar.gz
+echo "Internal prepairings..."
 cd ./systemc-2.3.2/src/sysc/datatypes/bit
-sed -i -e 's/mutable/ /g'
-cd ..
-cd ..
-cd ..
-cd ..
-mkdir /usr/local/systemc-2.3
+sed -i -e 's/mutable//' sc_bit_proxies.h
+cd ../../../..
+echo "Creating directory in \"root\"..."
+sudo mkdir /usr/local/systemc-2.3.2
+echo "Creating temporary directory..."
 mkdir objdir
 cd objdir
-../configure -prefix=/usr/local/systemc-2.3
-make
-make install
-export SYSTEMC_HOME=/usr/local/systemc-2.3/
-export LD_LIBRARY_PATH=/usr/local/systemc-2.3/lib-linux$digit
-echo >> "SYSTEMC_HOME=\"/usr/local/systemc-2.3/\"" >>  /etc/environment
-reboot now
+export CXX=g++
+echo "Setting configurations..."
+sudo ../configure -prefix=/usr/local/systemc-2.3.2
+echo "Starting making..."
+sudo make
+sudo make install
+echo "Making finished!"
+cd ../..
+echo "Uninstalling temporary directory..."
+sudo rm -rf systemc-2.3.2
+echo "Setting environment variables..."
+export SYSTEMC_HOME=/usr/local/systemc-2.3.2/
+export LD_LIBRARY_PATH=/usr/local/systemc-2.3.2/lib-linux$digit
+echo "Adding extra paths..."
+sudo echo "SYSTEMC_HOME=\"/usr/local/systemc-2.3.2/\"" | sudo tee -a /etc/environment #TODO eliminate the output of this string to terminal
+echo "System will be rebooted after 10 seconds"
+sleep 5; echo "5"; sleep 1; echo "4"; sleep 1; echo "3"; sleep 1; echo "2"; sleep 1; echo "1"
+sleep 1
+echo "REBOOTING"
+sudo reboot
